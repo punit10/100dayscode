72a5911647f078cb6040f76e40b2492f3de45bec
Punit Sharma <punitcs0067@gmail.com>
2025-09-18 17:38:25 +0530

    renamed variables for mor meaningful names!

--- day16/main.py
+++ day16/main.py
@@ -3,29 +3,30 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # User input turn off by "off"
-nescafe = CoffeeMaker()
-nescafe_menu = Menu()
-money = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
 
-state = True
-while state:
-    print("What would you like? (espresso/latte/cappuccino/): ")
+is_on = True
+while is_on:
+    print(f"What would you like? {menu.get_items()}: ")
     coffee_choice = input().lower()
     if coffee_choice == "off":
-        state = False
+        is_on = False
     elif coffee_choice == "report":
-        nescafe.report()
+        coffee_maker.report()
+        money_machine.report()
     # check if ordered drink is present in menu
-    elif coffee_choice in nescafe_menu.get_items().split("/"):
-        menu_item = nescafe_menu.find_drink(coffee_choice)
+    elif coffee_choice in menu.get_items().split("/"):
+        menu_item = menu.find_drink(coffee_choice)
         print(f"You have ordered: {menu_item.name}")
         print(f"Ingredients needed {menu_item.ingredients}")
 
         if menu_item is not None:
             # check if resources are enough to give drink
-            if nescafe.is_resource_sufficient(menu_item):
-                if money.make_payment(menu_item.cost):
-                    nescafe.make_coffee(menu_item)
+            if coffee_maker.is_resource_sufficient(menu_item):
+                if money_machine.make_payment(menu_item.cost):
+                    coffee_maker.make_coffee(menu_item)
 
     else:
-        menu_item = nescafe_menu.find_drink(coffee_choice)
+        menu_item = menu.find_drink(coffee_choice)
