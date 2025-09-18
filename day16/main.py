b2a3e4495c2209fef5ff8001fa95e7bd81aeab6c
Punit Sharma <punitcs0067@gmail.com>
2025-09-18 17:46:56 +0530

    Optimized code. Running !

--- day16/main.py
+++ day16/main.py
@@ -10,23 +10,20 @@ money_machine = MoneyMachine()
 is_on = True
 while is_on:
     print(f"What would you like? {menu.get_items()}: ")
-    coffee_choice = input().lower()
-    if coffee_choice == "off":
+    choice = input().lower()
+    if choice == "off":
         is_on = False
-    elif coffee_choice == "report":
+    elif choice == "report":
         coffee_maker.report()
         money_machine.report()
     # check if ordered drink is present in menu
-    elif coffee_choice in menu.get_items().split("/"):
-        menu_item = menu.find_drink(coffee_choice)
-        print(f"You have ordered: {menu_item.name}")
-        print(f"Ingredients needed {menu_item.ingredients}")
+    elif choice in menu.get_items().split("/"):
+        chosen_item = menu.find_drink(choice)
 
-        if menu_item is not None:
-            # check if resources are enough to give drink
-            if coffee_maker.is_resource_sufficient(menu_item):
-                if money_machine.make_payment(menu_item.cost):
-                    coffee_maker.make_coffee(menu_item)
+        # check if resources are enough to give drink
+        if coffee_maker.is_resource_sufficient(chosen_item):
+            if money_machine.make_payment(chosen_item.cost):
+                coffee_maker.make_coffee(chosen_item)
 
     else:
-        menu_item = menu.find_drink(coffee_choice)
+        menu_item = menu.find_drink(choice)
