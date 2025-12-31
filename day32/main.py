6e03d2bc397ebb6a82995387909a6228ea462d69
Punit Sharma <punitcs0067@gmail.com>
2025-12-31 12:03:37 +0530

    Added function to send quotes on monday vai email

--- day32/main.py
+++ day32/main.py
@@ -1,8 +1,19 @@
 import smtplib
 
 my_email = "vectorcampus1@gmail.com"
-password = "preM@1001"
-with smtplib.SMTP("smtp.gmail.com") as connection:
+password = input("Input details for sender email")
+
+# connection = smtplib.SMTP("smtp.gmail.com", port=587)
+# connection.starttls()
+# connection.login(user=my_email, password=password)
+# connection.sendmail(
+#     from_addr=my_email,
+#     to_addrs="punitvector@gmail.com",
+#     msg="Subject: Test email \n\n This is test email body",
+# )
+# connection.close()
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
     connection.sendmail(
---
+++ day32/monday_motivation.py
@@ -0,0 +1,22 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "vectorcampus1@gmail.com"
+MY_PASS = input("Enter details of sender email: ")
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 2:
+    with open("quotes.txt") as quotes_file:
+        all_quotes = quotes_file.readlines()
+        quote = random.choice(all_quotes)
+        print(quote)
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASS)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject: Monday Motivation \n\n {quote}"
+            )
