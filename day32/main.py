774dfcc15bc951f7479a3b1ad42a2abe8f0c8b93
Punit Sharma <punitcs0067@gmail.com>
2026-01-02 20:13:33 +0530

    Added starting file of project automated bday wisher

--- day32/main.py
+++ day32/main.py
@@ -18,6 +18,6 @@ with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.login(user=my_email, password=password)
     connection.sendmail(
         from_addr=my_email,
-        to_addrs="punitvector@gmail.com",
+        to_addrs=my_email,
         msg="Subject: Test email \n\n This is test email body"
     )
