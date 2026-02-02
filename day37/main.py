c8615df47345536364f99268ecea6aa994f722ca
Punit Sharma <punitcs0067@gmail.com>
2026-02-02 12:11:45 +0530

    Added pixels to create graph

--- day37/main.py
+++ day37/main.py
@@ -1,15 +1,47 @@
 import requests
 import os
+from datetime import datetime
 USERNAME = "punitsharma"
 TOKEN = os.environ.get("PIXELA_TOKEN")
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+GRAPH_ID = "graph1"
 user_params = {
     "token": TOKEN,
     "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-response.raise_for_status()
-print(response.json())
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# response.raise_for_status()
+# print(response.text)
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Walking graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "momiji"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# response = requests.post(url=graph_endpoint,
+#                          json=graph_config,
+#                          headers=headers,
+#                          verify=False
+#                         )
+# response.raise_for_status()
+# print(response.text)
+pixel_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_config = {
+    "date": "20260202",
+    "quantity": "4.0"
+}
+response = requests.post(
+    url=pixel_endpoint,
+    json=pixel_config,
+    headers=headers,
+    verify=False
+)
 print(response.text)
---
+++ day37/readme.md
@@ -0,0 +1,5 @@
+# This project is to Show git like graph to track progress of something
+# Using API post requests
+
+https://docs.pixe.la/entry/post-graph
+url to see the graph : https://pixe.la/v1/users/punitsharma/graphs/graph1.html
