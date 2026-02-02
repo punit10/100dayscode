71046a763fe46c31d717bfa88bd86ccc4dee4432
Punit Sharma <punitcs0067@gmail.com>
2026-02-02 20:27:56 +0530

    Added post pixel, update and delete pixel code working

--- day37/main.py
+++ day37/main.py
@@ -34,14 +34,62 @@ headers = {
 # response.raise_for_status()
 # print(response.text)
 pixel_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+# today = datetime(year=2026, month=1, day=30)
+today = datetime.now()
 pixel_config = {
-    "date": "20260202",
-    "quantity": "4.0"
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "3.0"
 }
-response = requests.post(
-    url=pixel_endpoint,
-    json=pixel_config,
+# response = requests.post(
+#     url=pixel_endpoint,
+#     json=pixel_config,
+#     headers=headers,
+#     verify=False
+# )
+# print(response.text)
+
+# post_multiple_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/pixels"
+# post_data = [{
+#     "date": datetime(year=2026, month=1, day=30).strftime("%Y%m%d"),
+#     "quantity": "6.0"
+# },
+# {
+#     "date": datetime(year=2026, month=1, day=29).strftime("%Y%m%d"),
+#     "quantity": "4.0"
+# },
+# {
+#     "date": datetime(year=2026, month=1, day=28).strftime("%Y%m%d"),
+#     "quantity": "2.0"
+# },
+# ]
+#
+# post_data_config = {"-": post_data}
+# response = requests.post(
+#     url=post_multiple_endpoint,
+#     json= post_data_config,
+#     headers=headers,
+#     verify=False
+# )
+# print(response.text)
+
+
+date_to_update = datetime(year=2026, month=1, day=30).strftime("%Y%m%d")
+update_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{date_to_update}"
+update_config = {
+    "quantity": "2.0"
+}
+response = requests.put(
+    url=update_endpoint,
+    json=update_config,
     headers=headers,
     verify=False
+)
+print(response.text)
+
+
+delete_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{date_to_update}"
+response = requests.delete(
+    url=delete_endpoint,
+    headers=headers,
 )
 print(response.text)
