e8b82f049c1fb171f4b37332f1353a2800c3d95f
Punit Sharma <punitcs0067@gmail.com>
2026-02-21 21:46:26 +0530

    Flast route practice

--- day54/hello.py
+++ day54/hello.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+@app.route("/greet")
+def greeting():
+    return "Welcome!"
+
+@app.route("/user/<name>")
+def greet(name):
+    return f"Hello, {name}!"
+
+@app.route("/user/<name>/<int:age>")
+def user(name, age):
+    return f"Hello, {name}! You are {age} years old."
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
