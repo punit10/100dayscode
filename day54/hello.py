0df2888a0150b3198aefb42757ba9f330ac83889
Punit Sharma <punitcs0067@gmail.com>
2026-02-23 16:44:19 +0530

    Added decorators to style html tags basic

--- day54/hello.py
+++ day54/hello.py
@@ -2,15 +2,30 @@ from flask import Flask
 
 # Create the application instance
 app = Flask(__name__)
+
+def make_bold(function):
+    def bold(*args, **kwargs):
+        return f"<b>{function(*args, **kwargs)}</b>"
+    return bold
+
+def make_underline(function):
+    def wrapper(*args, **kwargs):
+        return f"<u>{function(*args, **kwargs)}</u>"
+    return wrapper
+
 # Define a route for the default page
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 @app.route("/greet")
+@make_bold
+@make_underline
 def greeting():
     return "Welcome!"
 
 @app.route("/user/<name>")
+@make_underline
 def greet(name):
     return f"Hello, {name}!"
 
