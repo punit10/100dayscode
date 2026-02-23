dc5aa1e430d0a7c9880e654c80af0c0e633afe72
Punit Sharma <punitcs0067@gmail.com>
2026-02-23 18:42:23 +0530

    Added number guessing game with gifs

--- day54/hello.py
+++ day54/hello.py
@@ -2,6 +2,16 @@ from flask import Flask
 
 # Create the application instance
 app = Flask(__name__)
+class User():
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in:
+            function(args[0])
+        return wrapper
 
 def make_bold(function):
     def bold(*args, **kwargs):
@@ -12,6 +22,11 @@ def make_underline(function):
     def wrapper(*args, **kwargs):
         return f"<u>{function(*args, **kwargs)}</u>"
     return wrapper
+
+def make_italic(function):
+    def wrapper(*args, **kwargs):
+        return f"<u>{function(*args, **kwargs)}</u>"
+    return wrapper()
 
 # Define a route for the default page
 @app.route("/")
@@ -25,6 +40,7 @@ def greeting():
     return "Welcome!"
 
 @app.route("/user/<name>")
+@is_authenticated_decorator
 @make_underline
 def greet(name):
     return f"Hello, {name}!"
---
+++ day54/server.py
@@ -0,0 +1,24 @@
+# For the game Higher or Lower
+from flask import Flask
+import random
+app = Flask(__name__)
+
+GENERATED_NUMBER = random.randint(0,9)
+@app.route("/")
+def guess_number_home():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+@app.route("/<int:number>")
+def guess_number(number):
+    if number < GENERATED_NUMBER:
+        return "<h1 style='color: red'>Too low, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    elif number > GENERATED_NUMBER:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+if __name__ == "__main__":
+    app.run(debug=True)
