bdf3bce376d8a19dc9db120d6ae83a8f51f13873
Punit Sharma <punitcs0067@gmail.com>
2026-02-28 20:39:32 +0530

    Added dynamic footer year

--- day57/app.py
+++ day57/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import random
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    random_number = random.randint(1,10)
+    year = datetime.datetime.now().year
+    return render_template("index.html", num=random_number, year=year)
 
 
 if __name__ == "__main__":
