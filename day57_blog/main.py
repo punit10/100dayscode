6cf76f883eb759de312e2c6979c5fbf7f1304615
Punit Sharma <punitcs0067@gmail.com>
2026-03-01 17:25:35 +0530

    Made blog working taking post from api

--- day57_blog/main.py
+++ day57_blog/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 import requests
+import datetime
 
 app = Flask(__name__)
+year = datetime.datetime.now().year
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_endpoint = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_endpoint, verify=False)
+    blog_data = response.json()
+    return render_template("index.html",
+                           blog_data = blog_data,
+                           year=year)
 
 if __name__ == "__main__":
     app.run(debug=True)
