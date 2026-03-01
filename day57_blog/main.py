cc1b29561a9c0d8b8e956dc2196bca8b66e28444
Punit Sharma <punitcs0067@gmail.com>
2026-03-01 18:15:29 +0530

    Added post class Object oriented way of blog page

--- day57_blog/main.py
+++ day57_blog/main.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 import requests
 import datetime
-import post
+from post import Post
 
 app = Flask(__name__)
 year = datetime.datetime.now().year
 blog_endpoint = "https://api.npoint.io/c790b4d5cab58020d391"
 response = requests.get(blog_endpoint, verify=False)
 blog_data = response.json()
+post_objects = []
+for post in blog_data:
+    post_object = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_object)
 
 @app.route('/')
 def home():
@@ -17,9 +21,13 @@ def home():
 
 @app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def post(post_id):
-    return render_template("post.html",
-                           blog_data = blog_data,
-                           year=year,
-                           post_id=post_id)
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == post_id:
+            requested_post = blog_post
+            return render_template("post.html",
+                           post = requested_post,
+                           year=year)
+
 if __name__ == "__main__":
     app.run(debug=True)
