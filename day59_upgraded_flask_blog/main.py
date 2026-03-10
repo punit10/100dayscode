23949f1ff0e538d45802870a7228f446096fb0ac
Punit Sharma <punitcs0067@gmail.com>
2026-03-10 15:55:39 +0530

    Added contact and about pages and render dynamically, navbar redirect

--- day59_upgraded_flask_blog/main.py
+++ day59_upgraded_flask_blog/main.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def get_all_posts():
     return render_template("index.html", year=datetime.now().year)
 
+@app.route("/about")
+def about():
+    return render_template("about.html", year=datetime.now().year)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", year=datetime.now().year)
 
 if __name__ == "__main__":
     app.run(debug=True)
