ef2c5dc46dd2984a1b8249740be02162cc68a915
Punit Sharma <punitcs0067@gmail.com>
2026-03-10 15:39:56 +0530

    Included  code for header and footer and refactor

--- day59_upgraded_flask_blog/main.py
+++ day59_upgraded_flask_blog/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route("/")
-def home_page():
-    return render_template("index.html")
+def get_all_posts():
+    return render_template("index.html", year=datetime.now().year)
 
 
 if __name__ == "__main__":
