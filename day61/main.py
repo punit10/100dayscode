c6ca642c7345f228536cc0eaaeb8b591a141e625
Punit Sharma <punitcs0067@gmail.com>
2026-03-11 17:05:22 +0530

    GEtting form data and validating user

--- day61/main.py
+++ day61/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 '''
 Red underlines? Install the required packages first: 
@@ -19,21 +20,29 @@ This will install the packages from requirements.txt for this project.
 app = Flask(__name__)
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
+    email = StringField(label='Email', validators=[DataRequired()])
     # For best practice "label" property argument can be specified, without it also work
     #email = StringField('Email')
-    password = PasswordField(label='Password')
+    password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label='Login')
 
 app.secret_key = "some-secret-key"
+admin_email = "admin@email.com"
+admin_pass = "1234"
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if (login_form.email.data == admin_email and
+            login_form.password.data == admin_pass):
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
