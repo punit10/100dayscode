407480ac5437c2e40bf96eecf20502ab253d1a2a
Punit Sharma <punitcs0067@gmail.com>
2026-03-20 20:37:38 +0530

    Added dataabse for movies collection

--- day64/main.py
+++ day64/main.py
@@ -22,18 +22,77 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'very_secret_key'
 Bootstrap5(app)
 
 # CREATE DB
+all_movies = []
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+# configure the SQLite database, relative to the app instance folder
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-movies-collection.db"
+# initialize the app with the extension
+db.init_app(app)
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    year: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[float] = mapped_column(Float, nullable=False)
+    review: Mapped[float] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[float] = mapped_column(String(250), nullable=False)
 
+
+    def __init__(self,
+                 title,
+                 year,
+                 description,
+                 rating,
+                 ranking,
+                 review,
+                 img_url
+                 ):
+        self.title = title
+        self.year = year
+        self.description = description
+        self.rating = rating
+        self.ranking = ranking
+        self.review = review
+        self.img_url = img_url
+
+with app.app_context():
+    db.create_all()
+
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+second_movie = Movie(
+    title="Avatar The Way of Water",
+    year=2022,
+    description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+    rating=7.3,
+    ranking=9,
+    review="I liked the water.",
+    img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = db.session.execute(db.select(Movie).order_by(Movie.id)).scalars()
+    print(movies)
+    return render_template("index.html", movies=movies)
 
 
 if __name__ == '__main__':
