9129ace47cb80f61f20aaf42a16d948904852424
Punit Sharma <punitcs0067@gmail.com>
2026-04-02 11:57:48 +0530

    User Autherization Frontend done! logged in user to create, edit and delete post

--- main.py
+++ main.py
@@ -118,6 +118,13 @@ async def register_page(request: Request):
         {"title": "Register"},
     )
 
+@app.get("/account", include_in_schema=False)
+async def account_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "account.html",
+        {"title": "Account"},
+    )
 
 # routes for API in routes users.py
 
