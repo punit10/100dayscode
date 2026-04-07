10b330728dba8b46ae66ec69b924bcdd0cb36f2a
Punit Sharma <punitcs0067@gmail.com>
2026-04-07 20:13:18 +0530

    COMPLETED! Forgot and reset password frontend route and acc pages

--- main.py
+++ main.py
@@ -160,6 +160,27 @@ async def account_page(request: Request):
         {"title": "Account"},
     )
 
+
+## for forgot password and password reset routes
+@app.get("/forgot-password", include_in_schema=False)
+async def forgot_password_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "forgot_password.html",
+        {"title": "Forgot Password"},
+    )
+
+
+@app.get("/reset-password", include_in_schema=False)
+async def reset_password_page(request: Request):
+    response = templates.TemplateResponse(
+        request,
+        "reset_password.html",
+        {"title": "Reset Password"},
+    )
+    response.headers["Referrer-Policy"] = "no-referrer"
+    return response
+
 # routes for API in routes users.py
 
 # routes for Posts in posts.py
